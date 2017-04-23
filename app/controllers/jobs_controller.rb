9e332f73133737b79a2839d8ddf13c58fe21c470
lh850718 <lh850718@gmail.com>
2017-04-23 15:42:14 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,6 +6,10 @@ end
 
   def show
     @job=Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job already archived"
+      redirect_to root_path
+    end
   end
 
   def new
